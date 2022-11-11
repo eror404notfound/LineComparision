19b17e194aebc8eeb95b165faee854614c94b945
Ankit <ankitcool4370@gmail.com>
2022-11-11 20:07:46 +0530

    CalculationLength

--- LineComparision/Program.cs
+++ LineComparision/Program.cs
@@ -10,7 +10,24 @@ namespace LineComparision
     {
         static void Main(string[] args)
         {
-           Console.WriteLine("Welcome to the Line comparision Problem");
+            int x1;
+            int y1;
+
+            Console.WriteLine("Enter the 1st co-ordinates x1");
+            x1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the 1st co-ordinates y1");
+            y1 = Convert.ToInt32(Console.ReadLine());
+
+            int x2;
+            int y2;
+
+            Console.WriteLine("Enter the 1st co-ordinates x2");
+            x2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the 1st co-ordinates y2");
+            y2 = Convert.ToInt32(Console.ReadLine());
+
+            double length = Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2));
+            Console.WriteLine("length of the line is :- " + length);
 
             Console.ReadLine();
 
