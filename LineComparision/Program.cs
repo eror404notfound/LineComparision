829802c514930c57c01bb1c9fe4f4ce1de31ce44
Ankit <ankitcool4370@gmail.com>
2022-11-11 20:01:16 +0530

    LineComparision

---
+++ LineComparision/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LineComparision
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           Console.WriteLine("Welcome to the Line comparision Problem");
+
+            Console.ReadLine();
+
+        }
+    }
+}
