a9f450c462224e9c30c6252f3c77fc6afc803521
Ankit <ankitcool4370@gmail.com>
2022-11-20 15:28:11 +0530

    LineComparisionOPP

--- LineComparision/Program.cs
+++ LineComparision/Program.cs
@@ -10,34 +10,46 @@ namespace LineComparision
     {
         static void Main(string[] args)
         {
-            double[] length = new double[2];
-            for (int i = 0; i < 2; i++)
+            Console.WriteLine("Enter the first coordinates (x1 & y1) : ");
+            double x1 = Convert.ToInt32(Console.ReadLine());
+            double y1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the second coordinates (x2 & y2) : ");
+            double x2 = Convert.ToInt32(Console.ReadLine());
+            double y2 = Convert.ToInt32(Console.ReadLine());
+            double d1 = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));    //with using Pow function.
+            Console.WriteLine($"Co-ordinates provided are as follows (x1,y1),(x2,y2) : ({x1},{y1}),({x2},{y2})");
+            Console.WriteLine($"After calculating , Line distance value is : {d1}\n");
+            
+        }
+        public static void EqualsMethod(double line1Distance, double line2Distance)     //Equals()
+        {
+            bool EqualsStatus = line1Distance.Equals(line2Distance);
+            if (EqualsStatus)
             {
-                int x1;
-                int y1;
-                Console.WriteLine("Enter the 1st co-ordinates x1");
-                x1 = Convert.ToInt32(Console.ReadLine());
-                Console.WriteLine("Enter the 1st co-ordinates y1");
-                y1 = Convert.ToInt32(Console.ReadLine());
-
-                int x2;
-                int y2;
-
-                Console.WriteLine("Enter the 1st co-ordinates x2");
-                x2 = Convert.ToInt32(Console.ReadLine());
-                Console.WriteLine("Enter the 1st co-ordinates y2");
-                y2 = Convert.ToInt32(Console.ReadLine());
-
-                length[i] = Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2));
-                Console.WriteLine("length of the line is : " + length);
+                Console.WriteLine("As per the result , Length value of both Line 1 and Line 2 are EQUAL (using Equals())");
+            }
+            else
+            {
+                Console.WriteLine("As per the result ,Length value of Line 1 and Line 2 are NOT equal (using Equals())");
+            }
+        }
+        public static void CompareMethod(double line1Distance, double line2Distance)    //CompareTo()
+        {
+            int CompareToStatus = line1Distance.CompareTo(line2Distance);
+            if (CompareToStatus == 0)
+            {
+                Console.WriteLine("Both first and second lines are having Equal Length. (using CompareTo())");
+            }
+            else if (CompareToStatus < 0)
+            {
+                Console.WriteLine("Length of first line is Less than second line.  (using CompareTo())");
+            }
+            else
+            {
+                Console.WriteLine("Length of first line is Greater than second line.  (using CompareTo())");
             }
 
-            bool result = length[1].Equals(length[0]);
-            Console.WriteLine("Both lines are equals : " + result);
-            Console.ReadLine(); 
         }
-
-        
                
     }
 }
